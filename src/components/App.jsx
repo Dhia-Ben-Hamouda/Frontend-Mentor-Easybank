c427457226e8f35cc1fc0c9e8305c30646120a19
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-08-06 11:48:08 +0100

    more work on responsiveness

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import Home from "./Home.jsx";
 import About from "./About.jsx";
 import Footer from "./Footer.jsx";
 import Blog from "./Blog.jsx";
-import "./styles/styles.scss";
+import "../styles/styles.scss";
 
 const App = ()=>{
   return(
