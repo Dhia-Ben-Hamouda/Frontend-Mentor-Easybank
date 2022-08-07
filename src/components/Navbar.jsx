619c3bb0ffedd0c3e83ca3cdc53a2d9904605ecf
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-08-07 08:16:50 +0100

    more work on responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import logo from "../images/logo.svg"
 
 const Navbar = ()=>{
+
+  function handleClick()
+  {
+    document.querySelector(".bars").classList.toggle("clicked");
+    document.querySelector(".mobile-list").classList.toggle("opened");
+  }
+
   return(
     <>
       <nav>
         <img src={logo} alt="" />
-        <ul>
+        <ul className="desktop-list">
           <a href="#">Home</a>
           <a href="#">About</a>
           <a href="#">Contact</a>
@@ -14,6 +21,18 @@ const Navbar = ()=>{
           <a href="#">Careers</a>
         </ul>
         <button>Request invite</button>
+        <ul className="mobile-list opened">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Blog</a>
+          <a href="#">Careers</a>
+        </ul>
+        <div className="bars" onClick={handleClick}>
+          <div className="bar"></div>
+          <div className="bar mid"></div>
+          <div className="bar"></div>
+        </div>
       </nav>
     </>
   )
